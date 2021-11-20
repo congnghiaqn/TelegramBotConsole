8f9e1c72428cd79d41a4d829ef37fb8908165c97
Nguyễn Công Nghĩa <32643861+congnghiaqn@users.noreply.github.com>
2021-11-20 15:47:03 +0700

    Add project files.

---
+++ BotConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace TelegramBotConsole
+{
+    public static class BotConfig
+    {
+        public readonly static string BotToken = "2107916362:AAHx7OFWT_Qlvi-JMkNdEhjshFH8U2HBexg";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+
+namespace TelegramBotConsole;
+
+public static class Program
+{
+    private static TelegramBotClient? Bot;
+    public static async Task Main()
+    {
+        Bot = new TelegramBotClient(BotConfig.BotToken);
+        var me = await Bot.GetMeAsync();
+        Console.Title = me.Username;
+        using var cts = new CancellationTokenSource();
+        Bot.StartReceiving(new DefaultUpdateHandler(BotHandler.HandleUpdateAsync, BotHandler.HandleErrorAsync),
+                           cts.Token);
+        Console.WriteLine($"Start listening for @{me.Username}");
+        Console.ReadLine();
+        cts.Cancel();
+    }
+}
